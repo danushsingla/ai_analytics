5373c587e60ae8f894b3f63f509baf2e3ee2b6ff
danushsingla <dsingla340@gmail.com>
2025-12-20 11:50:16 -0500

    Added request ID function in case there is no crypto.randomUUID

--- app/routes/tracker.js
+++ app/routes/tracker.js
@@ -1,5 +1,18 @@
 
-(function () {
+(
+  function () {
+    // Creates a requestId in case there is no crypto.randomUUID available
+    function makeRequestId() {
+      if (window.crypto && crypto.randomUUID) {
+        return crypto.randomUUID();
+      }
+
+        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+          (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+        );
+    }
+
+
   // Grab the fetch to the backend
   var originalFetch = window.fetch;
 
@@ -9,8 +22,10 @@
     var options = arguments[1] || {};
     var method = options.method ? options.method : "GET";
 
+    console.log(url, method, options);
+
     // Add a request ID to track requests/responses together
-    var requestId = crypto.randomUUID();
+    var requestId = makeRequestId();
     
     // Get request body if there is one
     var requestBody = null;
